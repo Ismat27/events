59ab08d3a7099bb999ad81bae37ce5f4757870a8
ismat27 <adegbiteismail27@gmail.com>
2023-02-04 06:31:20 -0800

    added nav links

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 
@@ -14,12 +15,19 @@ export default function Home({events_categories}) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header>
+        <nav>
+          <Link href={'/'}>Home</Link>
+          <Link href={'/events'}>Events</Link>
+          <Link href={'/about-us'}>About us</Link>
+        </nav>
+      </header>
       <main className={styles.main}>
         {
           events_categories.map(cat => {
             const {id, title, description, image} = cat
             return (
-              <a key={id} href={`/events/${id}`}>
+              <Link key={id} href={`/events/${id}`}>
                 <Image 
                   src={image}
                   alt={title}
@@ -28,7 +36,7 @@ export default function Home({events_categories}) {
                 />
                 <h2>{title}</h2>
                 <p>{description}</p>
-              </a>
+              </Link>
             )
           })
         }
